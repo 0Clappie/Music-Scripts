986c57c2e46ac270e6e033a55aa13af63e9504f0
Devonte Jefferson <40244211+0Clappie@users.noreply.github.com>
2021-04-01 13:25:04 -0400

    Add files via upload

--- Music Alterations v3.py
+++ Music Alterations v3.py
@@ -2,10 +2,23 @@ import os
 import eyed3
 
 
-def files(path):
-    for file in os.listdir(path):
-        if os.path.isfile(os.path.join(path, file)):
-            yield file
+# def files(path):
+#    for file in os.listdir(path):
+#        if os.path.isfile(os.path.join(path, file)):
+#            yield file
+
+"""
+Turns out that the eyed3.load functionality just takes path as a parameter.
+"""
+
+
+# def path_getter(path):
+#    path = input("Please enter what path you'd like: ")
+#    print(path)
+
+"""
+All Cap!
+"""
 
 
 def title_alteration(music_artist_string):
@@ -16,10 +29,11 @@ def title_alteration(music_artist_string):
     return music_artist_string
 
 
-def main():
-    audio_files = eyed3.load(files('D:\\iTunes Music\\iTunes\\iTunes Media\\Music'))
+def main(path):
+    audio_files = eyed3.load(path)
     title_alteration(audio_files.tag.artist)
 
 
 if __name__ == '__main__':
-    main()
+    x = input("Please enter your path: ")
+    main(x)
